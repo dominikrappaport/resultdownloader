3e32126c9c7a8469c1fefaa7087a1ef799e9bd03
Dominik Rappaport <dominik@rappaport.at>
2025-10-25 12:36:11 +0200

    Version adjustment

--- src/resultdownloader/__init__.py
+++ src/resultdownloader/__init__.py
@@ -7,7 +7,7 @@ from RaceTimePro websites.
 
 from .downloader import download_results, RaceResultsDownloader
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["download_results", "RaceResultsDownloader"]
 __author__ = "Dominik Rappaport"
 __email__ = "dominik@rappaport.at"
